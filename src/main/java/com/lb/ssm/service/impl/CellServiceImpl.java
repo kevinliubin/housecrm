d49036a92bbad7f00a209b9048ccd34f0b6afa1c
kevinliubin <425089032@qq.com>
2016-10-24 15:30:31 +0800

    更新了新的实体映射

--- src/main/java/com/lb/ssm/service/impl/CellServiceImpl.java
+++ src/main/java/com/lb/ssm/service/impl/CellServiceImpl.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.github.pagehelper.PageHelper;
+import com.lb.ssm.mapper.CellExtendMapper;
 import com.lb.ssm.mapper.CellMapper;
 import com.lb.ssm.service.CellService;
 import com.lb.ssm.vo.Cell;
 import com.lb.ssm.vo.CellExample;
+import com.lb.ssm.vo.CellExtend;
 @Service
 public class CellServiceImpl implements CellService {
 	
@@ -17,8 +19,13 @@ public class CellServiceImpl implements CellService {
 	@Autowired
 	private CellMapper cellMapper;
 	
+	@Autowired
+	private CellExtendMapper cellExtendMapper;
+	
 	@Override
 	public List<Cell> getCellList(String limit,String offset) {
+		List<CellExtend> cellList = cellExtendMapper.getCellList();
+		System.out.println(cellList.size());
 		//实例化查询
 		CellExample example = new CellExample();
 		int pageindex=0;
