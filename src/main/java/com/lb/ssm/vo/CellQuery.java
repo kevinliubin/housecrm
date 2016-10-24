a4039c9d99270d41e02f9c413c507e2529992e74
kevinliubin <425089032@qq.com>
2016-10-24 15:31:32 +0800

    高价收快递发那个

---
+++ src/main/java/com/lb/ssm/vo/CellQuery.java
@@ -0,0 +1,25 @@
+package com.lb.ssm.vo;
+
+import java.util.List;
+
+/**
+ * 查询Cell返回的实体类
+ * @author hello
+ *
+ */
+public class CellQuery {
+	
+	private List<CellExtend> cellList;
+
+	public List<CellExtend> getCellList() {
+		return cellList;
+	}
+
+	public void setCellList(List<CellExtend> cellList) {
+		this.cellList = cellList;
+	}
+	
+	
+	
+	
+}
